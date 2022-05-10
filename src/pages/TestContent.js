ac038cc96f6186b6d2865a6e892ab4001a81bd0f
minhtuanqn <minhtuanqn320@gmail.com>
2022-05-10 20:56:30 +0700

    update-init

---
+++ src/pages/TestContent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Page from '../components/Page';
+
+const TestContent = () => (
+    // chỉ để test
+    <Page title="Dashboard">
+        <div>TestContent</div>
+    </Page>
+)
+export default TestContent
